317a3cef874771501ab50fa1c316c490f8379d86
SwapnilPadekar <slpadekar29@gmail.com>
2024-04-21 20:52:02 +0530

    This is my first commit with 3 files.

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Swapnil Padekar
+ * Subject: This is the first java program created without package.
+ */
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Swapnil");
+
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
